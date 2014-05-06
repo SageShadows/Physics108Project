859b44bd208cf543a5ad8613c9268d0f4ccc0b44
David Lam <SageShadows7@gmail.com>
2014-05-05 19:36:00 -0700

    Changed name to lockin_phase

--- instruments.py
+++ instruments.py
@@ -5,8 +5,13 @@ rm = visa.ResourceManager()
 multimeter = rm.get_instrument('GPIB0::2')
 lock_in = rm.get_instrument('GPIB0::6')
 
-#Sets 
-def phase(n):
+#Wraps the functions of the multimeter into easy to understand commands.
+
+def multimeter_voltage:
+	multimeter.write('MEAS:VOLT:DC? 10,0.003')
+	
+#Wraps the functions of the lock-in into easy to understand commands.
+def lockin_phase(n):
 	"""Sets the phase offset of the lock-in. -360.00 < n < 729.99 in degrees"""
 	lock_in.write('PHAS ' + str(n))
 
