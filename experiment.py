d426cc3432fdf7159dfa9748742fcb88a5183166
David Lam <SageShadows7@gmail.com>
2014-05-19 17:40:02 -0700

    Fixed the data array initialization.

--- experiment.py
+++ experiment.py
@@ -7,24 +7,23 @@ import instruments
 import os.path
 
 #Definitions of various mathematical functions and constants.
-temptovolt = lambda x: -0.0018*x+1.167 #Converts temperature to voltage for LN2 range.  
-volttotemp = lambda y: -555.4*y+648 #Converts voltage to temperature for LN2 range.  
-#temptovolt = lambda x: -0.002296*x+1.248 #Converts temperature to voltage for 300K range
-#volttotemp = lambda y: -435.6*y+543.7 #Converts voltage to temperature for 300K range
-tolerance = 0.01 #Tolerance in volts. 
+# temptovolt = lambda x: -0.0018*x+1.167 #Converts temperature to voltage for LN2 range.  
+# volttotemp = lambda y: -555.4*y+648 #Converts voltage to temperature for LN2 range.  
+temptovolt = lambda x: -0.002296*x+1.248 #Converts temperature to voltage for 300K range
+volttotemp = lambda y: -435.6*y+543.7 #Converts voltage to temperature for 300K range
+tolerance = 0.1 #Tolerance in volts. 
 savepath = 'C:\\Users\\David\\Desktop\\Dropbox\\School Work\\2013-2014\\Spring 2014\\PHYSICS 108'
 
 def recordData(minTemp, maxTemp, tempInterval, filename, phaseShift = 0, timeConstant = 11): 
 	""""Temperatures are given in Kelvin. The filename is a string that the data will be saved to. The phase shift is expermentially defined"""
 	initializeExperiment(phaseShift, timeConstant)
-	data = [['Temperature' 'Voltage' 'Phase']]
-	for i in np.linspace(minTemp, maxTemp, (maxTemp - minTemp)/tempInterval + 1):
-		refvolt = temptovolt(i);
-		instruments.keithley_setvoltage(refvolt)
-		thermalEq()
+	data = [['Temperature','Voltage','Phase']]
+	for temp in np.linspace(minTemp, maxTemp, (maxTemp - minTemp)/tempInterval + 1):
+		thermalEq(temp)
 		instruments.lockin_autosensitivity()
 		datapoint = instruments.lockin_measurement()
-		actualtemp = volttotemp(instruments.diodemultimeter_voltage())
+		actualtemp = volttotemp(-1*instruments.diodemultimeter_voltage())
+		print [actualtemp] + datapoint
 		data.append([actualtemp] + datapoint)
 	saveFile(filename, data)
 
@@ -33,7 +32,9 @@ def initializeExperiment(phaseShift, timeConstant):
 	instruments.lockin_phase(phaseShift)
 	instruments.lockin_timeconstant(timeConstant)
 
-def thermalEq(): 
+def thermalEq(temp): 
+	refvolt = temptovolt(temp)
+	instruments.keithley_setvoltage(refvolt)
 	counter = 0
 	while True: 
 		time.sleep(5)
@@ -54,4 +55,3 @@ def saveFile(filename, data):
 
 if __name__ == '__main__':
 	recordData(*sys.argv[1:])
-
